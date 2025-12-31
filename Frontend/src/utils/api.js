933ce68957a7d7625fc0478afb46b06b23f1c1d3
Balia Sahu <baliasahu7382@gmail.com>
2025-12-31 16:35:11 +0530

    Updated frontend code

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,8 +1,8 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://paste-3.onrender.com/";
 import axios from "axios";
 
 export const healthCheck = () => {
-  return fetch("http://localhost:8080/api/healthz");
+  return fetch("https://paste-3.onrender.com/api/healthz");
 };
 
 export const createPaste = (data) => {
